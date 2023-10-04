55b072587ba311da145a1371c23a665932a04f57
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-10-04 18:19:45 +0100

    Fixed Lint warning

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,6 +1,5 @@
 import { LinkBlock, StyledLink } from './AuthNav.styled';
 import { Hidden } from '@mui/material';
-import { useState } from 'react';
 import { MobileMenu } from 'components/MobileMenu/MobileMenu';
 
 export const AuthNav = () => {
